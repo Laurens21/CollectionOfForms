da55d0d52ab4c776bce12d2fd1298291037b9399
Laurens <1018265@idcollege.nl>
2019-12-10 13:38:58 +0100

    new changes

--- src/Form/PostType.php
+++ src/Form/PostType.php
@@ -5,6 +5,7 @@ namespace App\Form;
 use App\Entity\Post;
 use Symfony\Component\Form\AbstractType;
 use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Component\Form\Extension\Core\Type\CollectionType;
 use Symfony\Component\OptionsResolver\OptionsResolver;
 
 class PostType extends AbstractType
@@ -15,6 +16,12 @@ class PostType extends AbstractType
             ->add('title')
             ->add('description')
             ->add('category')
+            ->add('comments', CollectionType::class, [
+                'entry_type' => CommentType::class,
+                'entry_options' => ['label' => false],
+                'allow_add' => true,
+                'allow_delete' => true,
+            ])
         ;
     }
 
